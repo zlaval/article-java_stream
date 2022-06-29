7feffb57cf02f0647d036ef2404788f2e01cd94c
zlaval <zlaval@gmail.com>
2022-06-29 23:32:40 +0200

    feat: add compositions

--- src/main/java/functional/BuiltinFnInterfaces.java
+++ src/main/java/functional/BuiltinFnInterfaces.java
@@ -1,6 +1,7 @@
 package functional;
 
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
@@ -23,6 +24,21 @@ public class BuiltinFnInterfaces {
 
         Consumer<String> consumer = in -> System.out.println(in);
         consumer.accept("Hello World");
+
+        Predicate<Integer> gt5 = value -> value > 5;
+        Predicate<Integer> combined = lt10.and(gt5);
+
+        System.out.println(combined.test(5));
+        System.out.println(combined.test(10));
+        System.out.println(combined.test(7));
+
+
+        Function<String, Integer> convertStringToInt = Integer::valueOf;
+        Function<Integer, Integer> doubleInt = it -> it * 2;
+        Function<Integer, String> convertIntToString = Object::toString;
+
+        var composed = convertIntToString.compose(doubleInt).compose(convertStringToInt);
+        System.out.println(composed.apply("10"));
     }
 
     static void printWhenLT(int value, Predicate<Integer> testValue) {
